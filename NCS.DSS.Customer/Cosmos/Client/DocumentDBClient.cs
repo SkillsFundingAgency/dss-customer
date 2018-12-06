36878bac5f3ccf59fb975019e1abd79ffde3834c
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-12-06 14:42:56 +0000

    CDS-1004 - Remove Account Endpoint and Key

--- NCS.DSS.Customer/Cosmos/Client/DocumentDBClient.cs
+++ NCS.DSS.Customer/Cosmos/Client/DocumentDBClient.cs
@@ -13,12 +13,44 @@ namespace NCS.DSS.Customer.Cosmos.Client
             if (_documentClient != null)
                 return _documentClient;
 
-            _documentClient = new DocumentClient(new Uri(
-                ConfigurationManager.AppSettings["Endpoint"]),
-                ConfigurationManager.AppSettings["Key"]);
+            _documentClient = InitialiseDocumentClient();
 
             return _documentClient;
         }
-        
+
+        private static DocumentClient InitialiseDocumentClient()
+        {
+            string connectionString;
+
+            try
+            {
+                connectionString = ConfigurationManager.AppSettings["CustomerConnectionString"];
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            if(string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException();
+
+            var endPoint = connectionString.Split(new[] { "AccountEndpoint=" }, StringSplitOptions.None)[1]
+                .Split(';')[0]
+                .Trim();
+
+            if (string.IsNullOrWhiteSpace(endPoint))
+                throw new ArgumentNullException();
+
+            var key = connectionString.Split(new[] { "AccountKey=" }, StringSplitOptions.None)[1]
+                .Split(';')[0]
+                .Trim();
+
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException();
+
+            return new DocumentClient(new Uri(endPoint), key);
+        }
+
     }
 }
