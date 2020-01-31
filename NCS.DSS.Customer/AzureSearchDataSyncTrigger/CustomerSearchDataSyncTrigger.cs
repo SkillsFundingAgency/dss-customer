e982fb260e188ebc44a6449bb4161c0b41653aed
Huss <huss@anefiox.com>
2020-01-31 09:03:32 +0000

    Get tests to pass in pipeline

---
+++ NCS.DSS.Customer/AzureSearchDataSyncTrigger/CustomerSearchDataSyncTrigger.cs
@@ -0,0 +1,87 @@
+using DFC.Common.Standard.Logging;
+using DFC.Functions.DI.Standard.Attributes;
+using DFC.HTTP.Standard;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using NCS.DSS.Customer.Helpers;
+using NCS.DSS.Customer.ReferenceData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Document = Microsoft.Azure.Documents.Document;
+
+namespace NCS.DSS.Customer.AzureSearchDataSyncTrigger
+{
+    public static class CustomerSearchDataSyncTrigger
+    {        
+        [FunctionName("SyncDataForCustomerSearchTrigger")]
+        public static async Task Run(
+            [CosmosDBTrigger("customers", "customers", ConnectionStringSetting = "CustomerConnectionString",
+                LeaseCollectionName = "customers-leases", CreateLeaseCollectionIfNotExists = true)]
+            IReadOnlyList<Document> documents,
+            ILogger log,
+            [Inject]ILoggerHelper loggerHelper,
+            [Inject]IHttpRequestHelper httpRequestHelper)
+        {
+            var _correlationId = Guid.NewGuid();
+
+            loggerHelper.LogMethodEnter(log);            
+
+            SearchHelper.GetSearchServiceClient();
+
+            loggerHelper.LogInformationMessage(log, _correlationId, "get search service client");
+
+            var indexClient = SearchHelper.GetIndexClient();
+
+            loggerHelper.LogInformationMessage(log, _correlationId, "get index client");
+            
+            loggerHelper.LogInformationMessage(log, _correlationId, "Documents modified " + documents.Count);
+
+            if (documents.Count > 0)
+            {
+                var customers = documents.Select(doc => new Models.CustomerSearch()
+                    {
+                        CustomerId = doc.GetPropertyValue<Guid?>("id"),
+                        DateOfRegistration = doc.GetPropertyValue<DateTime?>("DateOfRegistration"),
+                        Title = doc.GetPropertyValue<Title>("Title"),
+                        GivenName = doc.GetPropertyValue<string>("GivenName"),
+                        FamilyName = doc.GetPropertyValue<string>("FamilyName"),
+                        DateofBirth = doc.GetPropertyValue<DateTime?>("DateofBirth"),
+                        Gender = doc.GetPropertyValue<Gender?>("Gender"),
+                        UniqueLearnerNumber = doc.GetPropertyValue<string>("UniqueLearnerNumber"),
+                        OptInUserResearch = doc.GetPropertyValue<bool?>("OptInUserResearch"),
+                        OptInMarketResearch = doc.GetPropertyValue<bool?>("OptInMarketResearch"),
+                        DateOfTermination = doc.GetPropertyValue<DateTime?>("DateOfTermination"),
+                        ReasonForTermination = doc.GetPropertyValue<ReasonForTermination?>("ReasonForTermination"),
+                        IntroducedBy = doc.GetPropertyValue<IntroducedBy?>("IntroducedBy"),
+                        IntroducedByAdditionalInfo = doc.GetPropertyValue<string>("IntroducedByAdditionalInfo"),
+                        LastModifiedDate = doc.GetPropertyValue<DateTime?>("LastModifiedDate"),
+                        LastModifiedTouchpointId = doc.GetPropertyValue<string>("LastModifiedTouchpointId")
+                    })
+                    .ToList();
+
+                var batch = IndexBatch.MergeOrUpload(customers);
+                
+                try
+                {
+                    log.LogInformation("attempting to merge docs to azure search");
+
+                    await indexClient.Documents.IndexAsync(batch);
+
+                    log.LogInformation("successfully merged docs to azure search");
+
+                }
+                catch (IndexBatchException e)
+                {
+                    loggerHelper.LogInformationMessage(log, _correlationId, string.Format("Failed to index some of the documents: {0}", 
+                        string.Join(", ", e.IndexingResults.Where(r => !r.Succeeded).Select(r => r.Key))));
+
+                    loggerHelper.LogException(log, _correlationId,  e);
+                }
+            }
+        }
+    }
+}
