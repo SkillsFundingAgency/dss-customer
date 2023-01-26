7df1951bc80d81c5b4b580ec7f4949244509a640
Joe Hudson-Small <Joehs@me.com>
2023-01-26 12:40:17 +0000

    .Net 6.0 Upgrade (DSS Release 2.0) (#75)
    * POC for .NET 6 upgrade
    
    * Upgraded function app runtime from v3 to v4
    
    * Updated Configuration setting for function app runtime
    
    * Updated Azure.Messaging.ServiceBus
    
    Add nuget Azure.Messaging.ServiceBus to hoist other version of package up to resolve issue with posting to service bus
    
    * Updated formatting of search uri due to new version of SDK
    
    * Removed unused search fields
    
    New version of SDK logs error when sending to azure search
    
    * Fix warning about pinned to unsupported version
    
    Co-authored-by: Amandeep Sandhu <purpleduckmedia@gmail.com>
    Co-authored-by: Amandeep Sandhu <github@purpleduckmedia.com>

--- NCS.DSS.Customer/Helpers/SearchHelper.cs
+++ NCS.DSS.Customer/Helpers/SearchHelper.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using Microsoft.Azure.Search;
+﻿using Azure;
+using Azure.Search.Documents;
+using System;
+
 
 namespace NCS.DSS.Customer.Helpers
 {
     public static class SearchHelper
-    {        
+    {
+        // https://learn.microsoft.com/en-us/azure/search/search-dotnet-sdk-migration-version-11
+
         private static readonly string SearchServiceName = Environment.GetEnvironmentVariable("SearchServiceName");
         private static readonly string SearchServiceKey = Environment.GetEnvironmentVariable("SearchServiceAdminApiKey");
         private static readonly string SearchServiceIndexName = Environment.GetEnvironmentVariable("CustomerSearchIndexName");
 
-        private static SearchServiceClient _serviceClient;
-        private static ISearchIndexClient _indexClient;
+        private static SearchClient _client;
 
-        public static SearchServiceClient GetSearchServiceClient()
+        public static SearchClient GetSearchServiceClient()
         {
-            if (_serviceClient != null)
-                return _serviceClient;
+            if (_client != null)
+                return _client;
 
-            _serviceClient = new SearchServiceClient(SearchServiceName, new SearchCredentials(SearchServiceKey));
+            _client = new SearchClient(new Uri($"https://{SearchServiceName}.search.windows.net"), SearchServiceIndexName, new AzureKeyCredential(SearchServiceKey));
 
-            return _serviceClient;
+            return _client;
         }
-        
-        public static ISearchIndexClient GetIndexClient()
-        {
-            if (_indexClient != null)
-                return _indexClient;
-
-            _indexClient = _serviceClient?.Indexes?.GetClient(SearchServiceIndexName);
-
-            return _indexClient;
-        }
-
+      
     }
 }
