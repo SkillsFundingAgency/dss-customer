51d80cf0dab3576df401efd025259fecd35e4dec
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2023-09-15 14:58:10 +0100

    Added RegEx validation for IntroducedByAdditionalInfo, SubcontractorId and LastModifiedTouchpointId fields

--- NCS.DSS.Customer.Tests/FunctionTests/PostCustomerHttpTriggerTests.cs
+++ NCS.DSS.Customer.Tests/FunctionTests/PostCustomerHttpTriggerTests.cs
@@ -63,7 +63,7 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
         public async Task PostCustomerHttpTrigger_ReturnsStatusCodeBadRequest_WhenTouchpointIdIsNotProvided()
         {
             // Arrange
-            _httpRequestHelper.Setup(x=>x.GetDssTouchpointId(_request)).Returns((string)null);
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns((string)null);
 
             // Act
             var result = await RunFunction(InValidId);
@@ -77,12 +77,12 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
         public async Task PostCustomerHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenCustomerHasFailedValidation()
         {
             // Arrange
-            _httpRequestHelper.Setup(x=>x.GetDssTouchpointId(_request)).Returns("0000000001");
-            _httpRequestHelper.Setup(x=>x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
+            _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
             _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
             var validationResults = new List<ValidationResult> { new ValidationResult("Customer Id is Required") };
             var val = new Mock<IValidate>();
-            val.Setup(x=>x.ValidateResource(It.IsAny<Models.Customer>(), It.IsAny<bool>())).Returns(validationResults);
+            val.Setup(x => x.ValidateResource(It.IsAny<Models.Customer>(), It.IsAny<bool>())).Returns(validationResults);
             _function = new PostCustomerHttpTrigger.Function.PostCustomerHttpTrigger(_resourceHelper.Object,
                 _httpRequestHelper.Object,
                 _httpResponseMessageHelper,
@@ -106,7 +106,7 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
             _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
             _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
             _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
-            _httpRequestHelper.Setup(x=>x.GetResourceFromRequest<Models.Customer>(It.IsAny<HttpRequest>())).ThrowsAsync(new JsonException());
+            _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(It.IsAny<HttpRequest>())).ThrowsAsync(new JsonException());
 
             // Act
             var result = await RunFunction(ValidCustomerId);
@@ -120,7 +120,7 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
         public async Task PostCustomerHttpTrigger_ReturnsStatusBadRequest_WhenRequestIsNotValid()
         {
             // Arrange
-            _postCustomerHttpTriggerService.Setup(x=>x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult<Models.Customer>(null));
+            _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult<Models.Customer>(null));
             _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
             _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
             _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
@@ -147,7 +147,7 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
         public async Task PostCustomerHttpTrigger_ReturnsStatusCodeCreated_WhenRequestIsValid()
         {
             // Arrange
-            _postCustomerHttpTriggerService.Setup(x=>x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
+            _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
             _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
             _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
             _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
@@ -161,6 +161,103 @@ namespace NCS.DSS.Customer.Tests.FunctionTests
                 _postCustomerHttpTriggerService.Object,
                 _jsonHelper,
                 _loggerHelper.Object);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+        }
+
+        [TestCase("<script>alert(1)</script>")]
+        [TestCase("Isobel.testing@dwp.gov.uk <script> Bridgewater JC")]
+        public async Task PostCustomerHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenIntroducedByAdditionalInfoRequestIsInValid(string additionalInfo)
+        {
+            // Arrange
+            _customer = new Models.Customer { IntroducedByAdditionalInfo = additionalInfo };
+
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
+            _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
+            _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
+
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.UnprocessableEntity, result.StatusCode);
+            var error = await result.Content.ReadAsStringAsync();
+
+            Assert.IsTrue(error.Contains("The field IntroducedByAdditionalInfo must match the regular expression"));
+        }
+
+        [TestCase("Universal Credit work coach holly")]
+        [TestCase("Isobel.testing@dwp.gov.uk Bridgewater PC")]
+        public async Task PostCustomerHttpTrigger_ReturnsStatusCodeCreated_WhenIntroducedByAdditionalInfoRequestIsValid(string additionalInfo)
+        {
+            // Arrange
+            _customer = new Models.Customer
+            {
+                GivenName = "Tester",
+                FamilyName = "Surname",
+                IntroducedBy = ReferenceData.IntroducedBy.CareersFairActivity,
+                PriorityGroups = new List<ReferenceData.PriorityCustomer> { ReferenceData.PriorityCustomer.AdultsWithSpecialEducationalNeedsAndOrDisabilities },
+                IntroducedByAdditionalInfo = additionalInfo
+            };
+            _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
+            _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
+            _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));            
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+        }
+
+        [TestCase("<script>alert(1)</script>")]
+        [TestCase("13231FDGD6")]
+        public async Task PostCustomerHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenSubcontractorIdRequestIsInValid(string subcontractorId)
+        {
+            // Arrange            
+            _httpRequestHelper.Setup(x => x.GetDssSubcontractorId(_request)).Returns(subcontractorId);            
+            _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
+            _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
+            _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.UnprocessableEntity, result.StatusCode);
+            var error = await result.Content.ReadAsStringAsync();
+
+            Assert.IsTrue(error.Contains("The field SubcontractorId must match the regular expression"));
+        }
+
+        [TestCase("12345678910")]
+        [TestCase("10001647")]
+        public async Task PatchCustomerHttpTrigger_ReturnsStatusCodeCreated_WhenSubcontractorIdRequestIsValid(string subcontractorId)
+        {
+            // Arrange
+            _customer = new Models.Customer
+            {
+                GivenName = "Tester",
+                FamilyName = "Surname",
+                IntroducedBy = ReferenceData.IntroducedBy.CareersFairActivity,
+                PriorityGroups = new List<ReferenceData.PriorityCustomer> { ReferenceData.PriorityCustomer.AdultsWithSpecialEducationalNeedsAndOrDisabilities }
+            };
+            _httpRequestHelper.Setup(x => x.GetDssSubcontractorId(_request)).Returns(subcontractorId);
+            _postCustomerHttpTriggerService.Setup(x => x.CreateNewCustomerAsync(It.IsAny<Models.Customer>())).Returns(Task.FromResult(_customer));
+            _httpRequestHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns("0000000001");
+            _httpRequestHelper.Setup(x => x.GetDssApimUrl(_request)).Returns("http://localhost:7071/");
+            _httpRequestHelper.Setup(x => x.GetResourceFromRequest<Models.Customer>(_request)).Returns(Task.FromResult(_customer));
 
             // Act
             var result = await RunFunction(ValidCustomerId);
