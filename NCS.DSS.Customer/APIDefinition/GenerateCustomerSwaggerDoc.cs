246616a64ad4d787234ffcbf9df473e474583219
amjones99 <59894100+amjones99@users.noreply.github.com>
2024-07-25 14:54:02 +0100

    Update nuget package and resolve swagger issue

--- NCS.DSS.Customer/APIDefinition/GenerateCustomerSwaggerDoc.cs
+++ NCS.DSS.Customer/APIDefinition/GenerateCustomerSwaggerDoc.cs
@@ -32,7 +32,7 @@ namespace NCS.DSS.Customer.APIDefinition
             if (string.IsNullOrEmpty(swagger))
                 return new NoContentResult();
 
-            return new OkObjectResult(new StringContent(swagger));
+            return new OkObjectResult(swagger);
         }
     }
 }
