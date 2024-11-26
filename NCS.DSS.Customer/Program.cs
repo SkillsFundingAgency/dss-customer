72557d298e2d720787dd6e5edd1771809bbf6915
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-11-26 13:19:22 +0000

    improve application insights logging

--- NCS.DSS.Customer/Program.cs
+++ NCS.DSS.Customer/Program.cs
@@ -4,6 +4,7 @@ using DFC.Swagger.Standard;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NCS.DSS.Customer.Cosmos.Helper;
 using NCS.DSS.Customer.Cosmos.Provider;
 using NCS.DSS.Customer.GetCustomerByIdHttpTrigger.Service;
@@ -38,6 +39,17 @@ namespace NCS.DSS.Customer
                     services.AddScoped<IServiceBusClient, ServiceBusClient>();
                     services.AddSingleton<IDocumentDBProvider, DocumentDBProvider>();
                     services.AddSingleton<IDynamicHelper, DynamicHelper>();
+                    services.Configure<LoggerFilterOptions>(options =>
+                    {
+                        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+                        // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+                        LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                            == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                        if (toRemove is not null)
+                        {
+                            options.Rules.Remove(toRemove);
+                        }
+                    });
                 })
                 .Build();
 
