cadebe0a52543580c744519b6f193a477db4d887
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-02 13:37:07 +0000

    bug fix again!

--- NCS.DSS.Customer/AzureSearchDataSyncTrigger/CustomerSearchDataSyncTrigger.cs
+++ NCS.DSS.Customer/AzureSearchDataSyncTrigger/CustomerSearchDataSyncTrigger.cs
@@ -20,7 +20,7 @@ namespace NCS.DSS.Customer.AzureSearchDataSyncTrigger
         public async Task RunAsync(
             [CosmosDBTrigger("customers", "customers", ConnectionStringSetting = "CustomerConnectionString",
                 LeaseCollectionName = "customers-leases", CreateLeaseCollectionIfNotExists = true)]
-            IReadOnlyList<JsonDocument> documents)
+            IReadOnlyList<Models.CustomerSearch> documents)
         {
             var correlationId = Guid.NewGuid();
 
@@ -36,33 +36,34 @@ namespace NCS.DSS.Customer.AzureSearchDataSyncTrigger
             _logger.LogInformation("{correlationId} Documents modified {count}", correlationId,documents.Count);
 
             if (documents.Count > 0)
-            {
-                var customers = documents.Select(doc => new Models.CustomerSearch()
-                {
-                    CustomerId = doc.RootElement.GetProperty("id").GetGuid(),
-                    DateOfRegistration = doc.RootElement.GetProperty("DateOfRegistration").GetDateTime(),
-                    Title =  Enum.Parse<Title>(doc.RootElement.GetProperty("Title").GetString()),
-                    GivenName = doc.RootElement.GetProperty("GivenName").GetString(),
-                    FamilyName = doc.RootElement.GetProperty("FamilyName").GetString(),
-                    DateofBirth = Enum.Parse<DateTime>(doc.RootElement.GetProperty("DateofBirth").GetString()),
-                    Gender = Enum.Parse<Gender>(doc.RootElement.GetProperty("Gender").GetString()),
-                    UniqueLearnerNumber = doc.RootElement.GetProperty("UniqueLearnerNumber").GetString(),
-                    OptInUserResearch = doc.RootElement.GetProperty("OptInUserResearch").GetBoolean(),
-                    OptInMarketResearch = doc.RootElement.GetProperty("OptInMarketResearch").GetBoolean(),
-                    DateOfTermination = Enum.Parse<DateTime>(doc.RootElement.GetProperty("DateOfTermination").GetString()),
-                    ReasonForTermination = Enum.Parse<ReasonForTermination>(doc.RootElement.GetProperty("ReasonForTermination").GetString()),
-                    IntroducedBy = Enum.Parse<IntroducedBy>(doc.RootElement.GetProperty("IntroducedBy").GetString()),
-                    IntroducedByAdditionalInfo = doc.RootElement.GetProperty("IntroducedByAdditionalInfo").GetString(),
-                    LastModifiedDate = Enum.Parse<DateTime>(doc.RootElement.GetProperty("LastModifiedDate").GetString()),
-                    LastModifiedTouchpointId = doc.RootElement.GetProperty("LastModifiedTouchpointId").GetString()
-                })
-                    .ToList();
-
-                var batch = IndexDocumentsBatch.MergeOrUpload(customers);
-
-
+            { 
                 try
                 {
+                    //var customers = documents.Select(doc => new Models.CustomerSearch()
+                    //{
+                    //    CustomerId = doc.RootElement.GetProperty("id").GetGuid(),
+                    //    DateOfRegistration = doc.RootElement.GetProperty("DateOfRegistration").GetDateTime(),
+                    //    Title = Enum.Parse<Title>(doc.RootElement.GetProperty("Title").GetString()),
+                    //    GivenName = doc.RootElement.GetProperty("GivenName").GetString(),
+                    //    FamilyName = doc.RootElement.GetProperty("FamilyName").GetString(),
+                    //    DateofBirth = Enum.Parse<DateTime>(doc.RootElement.GetProperty("DateofBirth").GetString()),
+                    //    Gender = Enum.Parse<Gender>(doc.RootElement.GetProperty("Gender").GetString()),
+                    //    UniqueLearnerNumber = doc.RootElement.GetProperty("UniqueLearnerNumber").GetString(),
+                    //    OptInUserResearch = doc.RootElement.GetProperty("OptInUserResearch").GetBoolean(),
+                    //    OptInMarketResearch = doc.RootElement.GetProperty("OptInMarketResearch").GetBoolean(),
+                    //    DateOfTermination = Enum.Parse<DateTime>(doc.RootElement.GetProperty("DateOfTermination").GetString()),
+                    //    ReasonForTermination = Enum.Parse<ReasonForTermination>(doc.RootElement.GetProperty("ReasonForTermination").GetString()),
+                    //    IntroducedBy = Enum.Parse<IntroducedBy>(doc.RootElement.GetProperty("IntroducedBy").GetString()),
+                    //    IntroducedByAdditionalInfo = doc.RootElement.GetProperty("IntroducedByAdditionalInfo").GetString(),
+                    //    LastModifiedDate = Enum.Parse<DateTime>(doc.RootElement.GetProperty("LastModifiedDate").GetString()),
+                    //    LastModifiedTouchpointId = doc.RootElement.GetProperty("LastModifiedTouchpointId").GetString()
+                    //})
+                    //    .ToList();
+
+                    var batch = IndexDocumentsBatch.MergeOrUpload(documents);
+
+
+
                     _logger.LogInformation("attempting to merge docs to azure search");
 
                     var results = await client.IndexDocumentsAsync(batch);
@@ -77,9 +78,9 @@ namespace NCS.DSS.Customer.AzureSearchDataSyncTrigger
                     _logger.LogInformation("successfully merged docs to azure search");
 
                 }
-                catch (RequestFailedException e)
+                catch (Exception e)
                 {
-                    _logger.LogError("{correlationId} Request failed Excpetion with {error}",correlationId, e.Message);
+                    _logger.LogError("{correlationId} Request failed Excpetion with {error}", correlationId, e.Message);
 
                 }
                 _logger.LogInformation("{functionName} existed", nameof(CustomerSearchDataSyncTrigger));
