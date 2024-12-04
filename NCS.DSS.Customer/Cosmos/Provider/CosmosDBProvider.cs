7ca138f53b72f540637f79033f239c951098aef9
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-04 10:58:35 +0000

    added additional logging and removed unused method

--- NCS.DSS.Customer/Cosmos/Provider/CosmosDBProvider.cs
+++ NCS.DSS.Customer/Cosmos/Provider/CosmosDBProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using Microsoft.Azure.Amqp.Framing;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Extensions.Logging;
 using NCS.DSS.Customer.Models;
@@ -28,13 +29,15 @@ namespace NCS.DSS.Customer.Cosmos.Provider
                 while (queryCust.HasMoreResults)
                 {
                     var response = await queryCust.ReadNextAsync();
+                    _logger.LogInformation("Customer Record found in Cosmos DB for {CustomerID}", customerId);
                     return true;
                 }
+                _logger.LogWarning("No Customer Record found with {CustomerID} in Cosmos DB", customerId);
                 return false;
             }
             catch (CosmosException ce)
             {
-                _logger.LogError("Failed to find the Customer Record in Cosmos DB {CustomerID}. Exception {Exception}", customerId, ce.Message);
+                _logger.LogError("Failed to find the Customer Record in Cosmos DB {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message,ce.StackTrace);
                 throw;
             }
             
@@ -48,14 +51,17 @@ namespace NCS.DSS.Customer.Cosmos.Provider
 
                 while (queryCust.HasMoreResults)
                 {
-                    var response = await queryCust.ReadNextAsync();
-                    return response.Resource.FirstOrDefault().DateOfTermination.HasValue;
+                    var response = await queryCust.ReadNextAsync();    
+                    var tDate = response.Resource.FirstOrDefault().DateOfTermination;
+                    _logger.LogInformation("Customer with {CustomerID} Have a termination date of {tDate} ", customerId,tDate);
+                    return tDate.HasValue;
                 }
+                _logger.LogWarning("No Customer Record found with {CustomerID} in Cosmos DB", customerId);
                 return false;
             }
             catch (CosmosException ce)
             {
-                _logger.LogError("Failed to get DateOfTermination for {CustomerID}. Exception {Exception}", customerId, ce.Message);
+                _logger.LogError("Failed to get DateOfTermination for {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message, ce.StackTrace);
                 throw;
             }
         }
@@ -72,6 +78,7 @@ namespace NCS.DSS.Customer.Cosmos.Provider
                     var response = await queryCust.ReadNextAsync();
                     customers.AddRange(response);
                 }
+                _logger.LogInformation("Number of Customer Records found {Count} ", customers.Count);
 
                 return customers.Count != 0 ? customers : null;
             }
@@ -92,13 +99,15 @@ namespace NCS.DSS.Customer.Cosmos.Provider
                 while (queryCust.HasMoreResults)
                 {
                     var response = await queryCust.ReadNextAsync();
+                    _logger.LogInformation("Customer Record found in Cosmos DB for {CustomerID}", customerId);
                     return response.Resource.FirstOrDefault();
                 }
+                _logger.LogWarning("No Customer Record found with {CustomerID} in Cosmos DB", customerId);
                 return null;
             }
             catch (CosmosException ce)
             {
-                _logger.LogError("Failed to find the Customer Record in Cosmos DB {CustomerID}. Exception {Exception}", customerId, ce.Message);
+                _logger.LogError("Failed to find the Customer Record in Cosmos DB {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message, ce.StackTrace);
                 throw;
             }            
         }
@@ -112,94 +121,151 @@ namespace NCS.DSS.Customer.Cosmos.Provider
                 while (queryCust.HasMoreResults)
                 {
                     var response = await queryCust.ReadNextAsync();
-                    return JsonSerializer.Serialize<Models.Customer>(response.Resource.FirstOrDefault());
+                    var customerJson = JsonSerializer.Serialize(response.Resource.FirstOrDefault());
+                    _logger.LogInformation("Customer Record found in Cosmos DB for {CustomerID}", customerId);
+                    return customerJson;
                 }
+                _logger.LogWarning("No Customer Record found with {CustomerID} in Cosmos DB", customerId);
                 return null;
             }
             catch (CosmosException ce)
             {
-                _logger.LogError("Failed to find the Customer Record for update in Cosmos DB {CustomerID}. Exception {Exception}", customerId, ce.Message);
+                _logger.LogError("Failed to find the Customer Record for update in Cosmos DB {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message,ce.StackTrace);
                 throw;
             }
         }
 
         public async Task<ItemResponse<Models.Customer>> CreateCustomerAsync(Models.Customer customer)
         {
-            return await _container.CreateItemAsync<Models.Customer>(customer, null);
+            try
+            {
+                var response = await _container.CreateItemAsync(customer, null);
+                if(response.StatusCode == HttpStatusCode.Created)
+                    _logger.LogInformation("Customer Record Created in Cosmos DB for {CustomerID}",customer.CustomerId);
+                return response;
+            }
+            catch (CosmosException ce)
+            {
+                _logger.LogError("Failed to Create Customer Record in Cosmos DB {CustomerID}. Exception {Exception}. {StackTrace}",customer.CustomerId, ce.Message,ce.StackTrace);
+                throw;
+            }
+            
 
         }
 
         public async Task<ItemResponse<Models.Customer>> UpdateCustomerAsync(string customerJson, Guid customerId)
         {
-            var customer = JsonSerializer.Deserialize<Models.Customer>(customerJson);
-            return await _container.ReplaceItemAsync(customer, customerId.ToString());
-        }
-
-        public async Task<List<Models.Subscriptions>> GetSubscriptionsByCustomerIdAsync(Guid? customerId)
-        {
-            var query = _container.GetItemLinqQueryable<Models.Subscriptions>()
-                .Where(x => x.CustomerId == customerId &&
-                            x.Subscribe)
-                .ToFeedIterator();
-
-            if (query == null)
-                return null;
-
-            var subscriptions = new List<Models.Subscriptions>();
-
-            while (query.HasMoreResults)
+            try
             {
-                var results = await query.ReadNextAsync();
-                subscriptions.AddRange(results);
+                var customer = JsonSerializer.Deserialize<Models.Customer>(customerJson);
+                var response = await _container.ReplaceItemAsync(customer, customerId.ToString());
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    _logger.LogInformation("Customer Record Updated in Cosmos DB for {CustomerID}", customer.CustomerId);
+                }
+                else
+                {
+                    _logger.LogInformation("Failed and returned {StatusCode} to Update Customer Record in Cosmos DB for {CustomerID}",response.StatusCode, customer.CustomerId);
+                }
+                    
+                return response;
             }
-
-            return subscriptions.Count != 0 ? subscriptions : null;
+            catch (CosmosException ce)
+            {
+                _logger.LogError("Failed to Update Customer Record in Cosmos DB {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message, ce.StackTrace);
+                throw;
+            }
+           
         }
 
         public async Task<Subscriptions> CreateSubscriptionsAsync(Models.Customer customer)
         {
             if (customer == null)
-                return null;
-
-            var subscription = new Subscriptions
             {
-                SubscriptionId = Guid.NewGuid(),
-                CustomerId = customer.CustomerId,
-                TouchPointId = customer.LastModifiedTouchpointId,
-                Subscribe = true,
-                LastModifiedDate = customer.LastModifiedDate,
-            };
+                return null;
+            }                
+            try
+            {
+                var subscription = new Subscriptions
+                {
+                    SubscriptionId = Guid.NewGuid(),
+                    CustomerId = customer.CustomerId,
+                    TouchPointId = customer.LastModifiedTouchpointId,
+                    Subscribe = true,
+                    LastModifiedDate = customer.LastModifiedDate,
+                };
 
-            if (!customer.LastModifiedDate.HasValue)
-                subscription.LastModifiedDate = DateTime.Now;
-            var response = await _container.CreateItemAsync(subscription, null);
-
-            return response.StatusCode == HttpStatusCode.Created ? (dynamic)response.Resource : (Guid?)null; 
+                if (!customer.LastModifiedDate.HasValue)
+                    subscription.LastModifiedDate = DateTime.Now;
+                var response = await _container.CreateItemAsync(subscription, null);
+                if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    _logger.LogInformation("Subscription Record Created in Cosmos DB for Customer with ID {CustomerID}", customer.CustomerId);
+                    return response.Resource;
+                }
+                else
+                {
+                    _logger.LogError("Failed to Create Subscription Record in Cosmos DB for Customer with ID {CustomerID}. Response Code {StatusCode}. ", customer.CustomerId, response.StatusCode);
+                    return null;
+                }                                    
+            }
+            catch (CosmosException ce)
+            {
+                _logger.LogError("Failed to Create Subscription Record in Cosmos DB for Customer with ID {CustomerID}. Exception {Exception}. {StackTrace}", customer.CustomerId, ce.Message, ce.StackTrace);
+                throw;
+            }
         }
 
         public async Task<DigitalIdentity> GetIdentityForCustomerAsync(Guid customerId)
         {
-            var query = _container.GetItemLinqQueryable<DigitalIdentity>()
-                .Where(x => x.CustomerId == customerId)
-                .ToFeedIterator();
-
-            if (query == null)
-                return null;
-            if (query.HasMoreResults)
+            try
             {
-                var digitalIdentity = await query.ReadNextAsync();
-
-                return digitalIdentity?.FirstOrDefault();
+                var query = _container.GetItemLinqQueryable<DigitalIdentity>()
+                                .Where(x => x.CustomerId == customerId)
+                                .ToFeedIterator();
+                if (query == null)
+                    return null;
+                if (query.HasMoreResults)
+                {
+                    var digitalIdentity = await query.ReadNextAsync();
+                    _logger.LogInformation("Digital Identity Record found in Cosmos DB for Customer with ID {CustomerID}", customerId);
+                    return digitalIdentity?.FirstOrDefault();
+                }
+                else
+                {
+                    _logger.LogError("Failed to Retrieve Digital Identity Record in Cosmos DB for Customer with ID {CustomerID}", customerId);
+                    return null;
+                }
             }
-            else
+            catch (CosmosException ce)
             {
-                return null;
+                _logger.LogError("Failed to Retrieve Digital Identity Record in Cosmos DB for Customer with ID {CustomerID}. Exception {Exception}. {StackTrace}", customerId, ce.Message, ce.StackTrace);
+                throw;
             }
+            
         }
 
         public async Task<DigitalIdentity> UpdateIdentityAsync(DigitalIdentity digitalIdentity)
         {
-            return await _container.ReplaceItemAsync(digitalIdentity, digitalIdentity.IdentityID.ToString());
+            try
+            {
+                var response = await _container.ReplaceItemAsync(digitalIdentity, digitalIdentity.IdentityID.ToString());
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    _logger.LogInformation("Digital Identity Record Updated in Cosmos DB for {DigiID}", digitalIdentity.IdentityID);
+                }
+                else
+                {
+                    _logger.LogInformation("Failed and returned {StatusCode} to Update Digital Identity Record in Cosmos DB for {DigiID}", response.StatusCode, digitalIdentity.IdentityID);
+                }
+                return response.Resource;
+            }
+            catch (CosmosException ce)
+            {
+                _logger.LogError("Failed to Update Digital Identity Record in Cosmos DB with ID {DigId}. Exception {Exception}. {StackTrace}", digitalIdentity.IdentityID, ce.Message, ce.StackTrace);
+
+                throw;
+            }
         }
     }
 }
--- NCS.DSS.Customer/Cosmos/Provider/ICosmosDBProvider.cs
+++ NCS.DSS.Customer/Cosmos/Provider/ICosmosDBProvider.cs
@@ -12,7 +12,6 @@ namespace NCS.DSS.Customer.Cosmos.Provider
         Task<string> GetCustomerByIdForUpdateAsync(Guid customerId);
         Task<ItemResponse<Models.Customer>> CreateCustomerAsync(Models.Customer customer);
         Task<ItemResponse<Models.Customer>> UpdateCustomerAsync(string customerJson, Guid customerId);
-        Task<List<Subscriptions>> GetSubscriptionsByCustomerIdAsync(Guid? customerId);
         Task<Subscriptions> CreateSubscriptionsAsync(Models.Customer customer);
         Task<DigitalIdentity> GetIdentityForCustomerAsync(Guid customerId);
         Task<DigitalIdentity> UpdateIdentityAsync(DigitalIdentity digitalIdentity);
