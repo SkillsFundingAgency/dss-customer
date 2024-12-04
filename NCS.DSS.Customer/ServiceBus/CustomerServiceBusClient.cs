ee3be2569fb72044c0ef5410640799e54c2575b9
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-04 11:12:42 +0000

    additional logging added

--- NCS.DSS.Customer/ServiceBus/CustomerServiceBusClient.cs
+++ NCS.DSS.Customer/ServiceBus/CustomerServiceBusClient.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Logging;
 using NCS.DSS.Customer.Cosmos.Provider;
 using NCS.DSS.Customer.Models;
 using Newtonsoft.Json;
@@ -9,68 +10,97 @@ namespace NCS.DSS.Customer.ServiceBus
     public class CustomerServiceBusClient : ICustomerServiceBusClient
     {
         private readonly ICosmosDBProvider _cosmosDBProvider;
+        private readonly ILogger<CustomerServiceBusClient> _logger;
         public readonly string QueueName = Environment.GetEnvironmentVariable("QueueName");
         public readonly string ServiceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
         private readonly ServiceBusClient _serviceBusClient;
 
-        public CustomerServiceBusClient(ICosmosDBProvider cosmosDBProvider, ServiceBusClient serviceBusClient)
+        public CustomerServiceBusClient(ICosmosDBProvider cosmosDBProvider, ServiceBusClient serviceBusClient, ILogger<CustomerServiceBusClient> logger)
         {
             _cosmosDBProvider = cosmosDBProvider;
             _serviceBusClient = serviceBusClient;
+            _logger = logger;
         }
 
         public async Task SendPostMessageAsync(Models.Customer customer, string reqUrl)
         {
-            var serviceBusSender = _serviceBusClient.CreateSender(QueueName);
-
-            var messageModel = new MessageModel()
+            try
             {
-                TitleMessage = "New Customer record {" + customer.CustomerId + "} added at " + DateTime.UtcNow,
-                CustomerGuid = customer.CustomerId,
-                LastModifiedDate = customer.LastModifiedDate,
-                URL = reqUrl + "/" + customer.CustomerId,
-                IsNewCustomer = false,
-                TouchpointId = customer.LastModifiedTouchpointId
-            };
+                _logger.LogInformation("Attempting to Create Sender for Service Bus Client");
+                var serviceBusSender = _serviceBusClient.CreateSender(QueueName);
+                _logger.LogInformation("Preparing Message for Service Bus");
+                var messageModel = new MessageModel()
+                {
+                    TitleMessage = "New Customer record {" + customer.CustomerId + "} added at " + DateTime.UtcNow,
+                    CustomerGuid = customer.CustomerId,
+                    LastModifiedDate = customer.LastModifiedDate,
+                    URL = reqUrl + "/" + customer.CustomerId,
+                    IsNewCustomer = false,
+                    TouchpointId = customer.LastModifiedTouchpointId
+                };
 
-            var msg = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel)))
+                var msg = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel)))
+                {
+                    ContentType = "application/json",
+                    MessageId = customer.CustomerId + " " + DateTime.UtcNow
+                };
+
+                _logger.LogInformation("Attempting to Auto Subscribe Customer with ID {CustomerID}",customer.CustomerId);
+                await AutoSubscribeCustomer(customer);
+
+                _logger.LogInformation("Attempting to Send Service Bus Message for Customer with ID {CustomerID}", customer.CustomerId);
+                await serviceBusSender.SendMessageAsync(msg);
+                _logger.LogInformation("Service Bus Message for Customer with ID {CustomerID} has been sent successfully", customer.CustomerId);
+
+            }
+            catch (Exception ex)
             {
-                ContentType = "application/json",
-                MessageId = customer.CustomerId + " " + DateTime.UtcNow
-            };
-
-            await AutoSubscribeCustomer(customer);
-            await serviceBusSender.SendMessageAsync(msg);
+                _logger.LogInformation("Failed to Send Service Bus Message for Customer with ID {CustomerID}. Exception Raised with {Message}. {StackTrace}", customer.CustomerId, ex.Message,ex.StackTrace);
+                throw;
+            }            
         }
 
         public async Task SendPatchMessageAsync(CustomerPatch customerPatch, Guid customerId, string reqUrl)
         {
-            var serviceBusSender = _serviceBusClient.CreateSender(QueueName);
-
-            var messageModel = new MessageModel
+            try
             {
-                TitleMessage = "Outcome record modification for {" + customerId + "} at " + DateTime.UtcNow,
-                CustomerGuid = customerId,
-                LastModifiedDate = customerPatch.LastModifiedDate,
-                URL = reqUrl,
-                IsNewCustomer = false,
-                TouchpointId = customerPatch.LastModifiedTouchpointId,
-                IsDigitalAccount = customerPatch.IsDigitalAccount,
-                UpdateDigitalIdentity = customerPatch.UpdateDigitalIdentity,
-                FirstName = customerPatch.GivenName,
-                LastName = customerPatch.FamilyName,
-                IdentityStoreId = customerPatch.IdentityStoreId,
-                DeleteDigitalIdentity = customerPatch.DeleteDigitalIdentity
-            };
+                _logger.LogInformation("Attempting to Create Sender for Service Bus Client");
+                var serviceBusSender = _serviceBusClient.CreateSender(QueueName);
+                _logger.LogInformation("Preparing Message for Service Bus");
 
-            var msg = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel)))
+                var messageModel = new MessageModel
+                {
+                    TitleMessage = "Outcome record modification for {" + customerId + "} at " + DateTime.UtcNow,
+                    CustomerGuid = customerId,
+                    LastModifiedDate = customerPatch.LastModifiedDate,
+                    URL = reqUrl,
+                    IsNewCustomer = false,
+                    TouchpointId = customerPatch.LastModifiedTouchpointId,
+                    IsDigitalAccount = customerPatch.IsDigitalAccount,
+                    UpdateDigitalIdentity = customerPatch.UpdateDigitalIdentity,
+                    FirstName = customerPatch.GivenName,
+                    LastName = customerPatch.FamilyName,
+                    IdentityStoreId = customerPatch.IdentityStoreId,
+                    DeleteDigitalIdentity = customerPatch.DeleteDigitalIdentity
+                };
+
+                var msg = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel)))
+                {
+                    ContentType = "application/json",
+                    MessageId = customerId + " " + DateTime.UtcNow
+                };
+
+                _logger.LogInformation("Attempting to Send Service Bus Message for Customer with ID {CustomerID}", customerId);
+                await serviceBusSender.SendMessageAsync(msg);
+                _logger.LogInformation("Service Bus Message for Customer with ID {CustomerID} has been sent successfully", customerId);
+
+
+            }
+            catch (Exception ex)
             {
-                ContentType = "application/json",
-                MessageId = customerId + " " + DateTime.UtcNow
-            };
-
-            await serviceBusSender.SendMessageAsync(msg);
-
+                _logger.LogInformation("Failed to Send Service Bus Message for Customer with ID {CustomerID}. Exception Raised with {Message}. {StackTrace}", customerId, ex.Message, ex.StackTrace);
+                throw;
+            }
         }
 
         public class MessageModel
