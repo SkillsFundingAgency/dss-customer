21af5ba489994b89c8d9186c5ddeb438bdd36400
mlarkowski-DFE <178817151+mlarkowski-DFE@users.noreply.github.com>
2025-10-01 15:27:37 +0100

    removed microsoft packages from app insights

--- NCS.DSS.Customer/Program.cs
+++ NCS.DSS.Customer/Program.cs
@@ -88,6 +88,7 @@ namespace NCS.DSS.Customer
                     {
                         LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
                             == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                        options.AddFilter("Microsoft.AspNetCore", LogLevel.Warning);
                         if (toRemove is not null)
                         {
                             options.Rules.Remove(toRemove);
